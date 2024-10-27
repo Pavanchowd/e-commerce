7c257449f7d4305b7af98dcdf2f390311cbec0f3
pavan kumar <pavanchowdary0100@gmail.cpm>
2024-10-27 21:16:22 +0530

    https://github.com/Pavanchowd/e-commerce/commit/c25563cb0aed78dc8a248c77c1362ae5693e7a26

--- mern-crash-course-master/backend/config/db.js
+++ mern-crash-course-master/backend/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-
 export const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-		process.exit(1); // process code 1 code means exit with failure, 0 means success
-	}
+  try {
+    console.log("Mongo URI:", process.env.MONGO_URI); // Log the URI for debugging
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
--- mern-crash-course-master/backend/server.js
+++ mern-crash-course-master/backend/server.js
@@ -18,13 +18,13 @@ app.use(express.json()); // allows us to accept JSON data in the req.body
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server started at http://localhost:" + PORT);
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
 });
